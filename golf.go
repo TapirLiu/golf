af54a84da6564519e57952feb8c957e71c49fdba
Tapir <9266865+TapirLiu@users.noreply.github.com>
2018-09-19 11:33:26 -0400

    add auto open browser option

--- golf.go
+++ golf.go
@@ -2,44 +2,30 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"net/http"
-	"path/filepath"
-	"os"
+	"log"
 	"net"
+	"net/http"
+	"os"
+	"os/exec"
+	"runtime"
 	"time"
 )
 
 var (
-	port = flag.String("port", "9999", "listening port")
+	port    = flag.String("port", "9999", "listening port")
+	browser = flag.Bool("b", false, "open first page in a browser automatically")
 )
 
 func main() {
 	flag.Parse()
-	
-	pwd, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	log.SetFlags(0)
+
+	pwd, err := os.Getwd()
 	if err != nil {
-		fmt.Println("Can't get current path.")
+		log.Println("Can't get current path.")
 		os.Exit(1)
 	}
 
-	go func() {
-		time.Sleep(time.Second)
-		
-		fmt.Println("Serving folder:")
-		fmt.Println("   " + pwd)
-		fmt.Println("Running at:")
-		fmt.Println("   http://localhost:" + *port)
-	
-		if addrs, err := net.InterfaceAddrs(); err == nil {
-			for _, a := range addrs {
-				if ipnet, ok := a.(*net.IPNet); ok && ipnet.IP.To4() != nil {
-					fmt.Println("   http://" + ipnet.IP.String() + ":" + *port)
-				}
-			}
-		}
-	}()
-	
 	// http://stackoverflow.com/questions/33880343/go-webserver-dont-cache-files-using-timestamp
 	var epoch = time.Unix(0, 0).Format(time.RFC1123)
 	var noCacheHeaders = map[string]string{
@@ -56,28 +42,58 @@ func main() {
 		"If-Range",
 		"If-Unmodified-Since",
 	}
-	
+
 	NoCacheHandler := func(h http.Handler) http.HandlerFunc {
 		return func(w http.ResponseWriter, r *http.Request) {
-			// Delete any ETag headers that may have been set
 			for _, v := range etagHeaders {
 				if r.Header.Get(v) != "" {
 					r.Header.Del(v)
 				}
 			}
-
-			// Set our NoCache headers
 			for k, v := range noCacheHeaders {
 				w.Header().Set(k, v)
 			}
-			
-			// Serve with the actual handler.
 			h.ServeHTTP(w, r)
 		}
-	} 
+	}
+
+	go func() {
+		time.Sleep(time.Second)
+
+		log.Println("Serving folder:")
+		log.Println("   " + pwd)
+		log.Println("Running at:")
+		log.Println("   http://localhost:" + *port)
+
+		if addrs, err := net.InterfaceAddrs(); err == nil {
+			for _, a := range addrs {
+				if ipnet, ok := a.(*net.IPNet); ok && ipnet.IP.To4() != nil {
+					log.Println("   http://" + ipnet.IP.String() + ":" + *port)
+				}
+			}
+		}
+
+		if !*browser {
+			return
+		}
+
+		// https://stackoverflow.com/questions/39320371/how-start-web-server-to-open-page-in-browser-in-golang
+		var cmd string
+		var args []string
+		switch runtime.GOOS {
+		case "windows":
+			cmd = "cmd"
+			args = []string{"/c", "start"}
+		case "darwin":
+			cmd = "open"
+		default: // "linux", "freebsd", "openbsd", "netbsd"
+			cmd = "xdg-open"
+		}
+		_ = exec.Command(cmd, append(args, "http://localhost:"+*port)...).Start()
+	}()
 
 	handler := NoCacheHandler(http.FileServer(http.Dir(pwd)))
 	if err = http.ListenAndServe(":"+*port, handler); err != nil {
-		fmt.Printf("Failed to start server: %v\n", err)
+		log.Printf("Failed to start server: %v\n", err)
 	}
 }
